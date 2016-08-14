6f9372d7fbbeec86cbc4b2b7762c52f12eb9e3fb
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-08-14 22:29:07 +0100

    added stock

---
+++ models/stock.rb
@@ -0,0 +1,41 @@
+require('pg')
+require_relative('../db/sql_runner')
+require_relative('album')
+
+class Stock
+
+  attr_reader(:id, :album_id, :format, :stock_level, :stock_health, :buy_price, :sell_price)
+
+  def self.all()
+    sql = "SELECT * FROM stocks"
+    stocks = SqlRunner.run(sql)
+    result = stocks.map {|stock| Stock.new(stock)}
+    return result
+  end
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @album_id = options['album_id'].to_i
+    @stock_level = options['stock_level'].to_i
+    @stock_health = options['stock_health']
+    @buy_price = options['buy_price'].to_i
+    @sell_price = options['sell_price'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO stocks (album_id, format, stock_level, stock_health, buy_price, sell_price) VALUES 
+    ( #{@album_id}, #{@stock_level}, #{'@stock_health'}, #{@buy_price}, #{@sell_price}) RETURNING *"
+    stock = SqlRunner.run(sql).first
+    @id = stock['id']
+  end
+
+  def album()
+    sql = "SELECT * FROM albums where id = #{@album_id}"
+    album = SqlRunner.run(sql).first
+    result = Album.new(album)
+    return result
+  end
+
+
+
+end

334b244f647a1f8098548b81f7ed83ca768f6f7f
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-08-16 14:10:15 +0100

    obligatory 30min commit

--- main.rb
+++ main.rb
@@ -5,11 +5,25 @@ require_relative('./models/artist')
 require_relative('./models/album')
 require_relative('./models/stock')
 
+#HOME
+get '/' do
+  erb(:home)
+end
 
 # MAIN INDEX(INVENTORY)
 get '/index' do
   @stocks = Stock.all()
   erb(:index)
+end
+
+# ADD/DISPLAY RECORDS
+get '/add' do
+  erb(:add_records)
+end
+
+# ADD/DISPLAY RECORDS
+get '/display' do
+  erb(:display_records)
 end
 
 #UPDATE
